814b010a361dbbcc4fbd4d2f4fe62ecddbb95341
HateBaozi <terence.ct@gmail.com>
2022-10-31 14:55:35 +0800

    增加生活热水控制和传感器，去掉无用的实体

--- custom_components/bestway/const.py
+++ custom_components/bestway/const.py
@@ -9,3 +9,6 @@ CONF_API_ROOT_EU = "https://euapi.gizwits.com"
 CONF_API_ROOT_US = "https://usapi.gizwits.com"
 CONF_USER_TOKEN = "user_token"
 CONF_USER_TOKEN_EXPIRY = "user_token_expiry"
+
+DHW_ON = "ON"
+DHW_OFF = "OFF"
